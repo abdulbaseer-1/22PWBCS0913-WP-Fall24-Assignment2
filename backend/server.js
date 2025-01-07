3d2689fef53a2a97b8d9c2552185272a71a4a6fd
Abdul Baseer <abdulbaseer.s130@gmail.com>
2025-01-08 03:06:03 +0500

    changes made

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import CORS from './middleware/CORS.js';
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // const server = () => { // not removing b/c maybe i'll need it later for vercel and I am lazy so no rewrite.
     const app = express();
     const PORT = 8080;
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    app.use(express.static(path.join(__dirname, 'dist')));
+
 
     //middlewares
     app.use(CORS);
@@ -13,6 +21,11 @@ import userRoutes from './routes/userRoutes.js';
 
     //routes
     app.use('/api', userRoutes);
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+      });
+
 
     //mongo connection
     const URI = 'mongodb+srv://khanabdulbaseerkhanyousafzai:yLBxFcRNtnzWl3eK@cluster0.duqoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
