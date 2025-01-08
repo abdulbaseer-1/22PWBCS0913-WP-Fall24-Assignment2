e50948ccfa27f39ed45fdf240a5280b70dc56667
Abdul Baseer <abdulbaseer.s130@gmail.com>
2025-01-09 02:06:43 +0500

    cors path corrected

--- server/middleware/CORS.js
+++ server/middleware/CORS.js
@@ -1,10 +1,10 @@
 import cors from 'cors';
 
-const CORS = cors({ // itsself a middleware, no need for (req, res, next)
-        origin: 'https://assignment2-frontend-psi.vercel.app',
-        methods:['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-        allowedeaders:['Content-Type', 'Authorization'],
-        credentials:true,
-    });
+const CORS = cors({ 
+    origin: ['https://assignment2-frontend-psi.vercel.app', 'https://assignment2-frontend-ovmho5zen-abdulbaseer-1s-projects.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+});
 
 export default CORS;
