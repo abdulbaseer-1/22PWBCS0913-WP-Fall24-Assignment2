471bad62aef3eb738d33272a28b9147e9404a384
Abdul Baseer <abdulbaseer.s130@gmail.com>
2025-01-09 00:11:54 +0500

    some changes,

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../dist',  // Build output will go to the root/dist
+    emptyOutDir: true      // Ensures the output directory is cleaned before building
+  }
 })
